e7ac7d4d28d23d84e33cff66dbfea4e0966b793c
FinkVinicius <viniciusfink2001@gmail.com>
2026-05-09 00:56:46 -0300

    Comentários.

--- script.js
+++ script.js
@@ -6,30 +6,31 @@ menuBtn.addEventListener("click", function() {
     menu.classList.toggle("ativo");
 });
 
-/* formulario */
+/* validação do formulario */
 const form = document.getElementById("formContato");
 const status = document.getElementById("statusEnvio");
 
 form.addEventListener("submit", function(event) {
-
+    // Impede o recarregamento da página ao clicar no botão de enviar
     event.preventDefault();
-
+    
+    // pega os valores dos inputs e remove espaços extras
     const nome = document.getElementById("nome").value.trim();
     const email = document.getElementById("email").value.trim();
     const mensagem = document.getElementById("mensagem").value.trim();
-
+    //molde pra validação
     const emailValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
+    // verifica se tem algum campo vazio 
     if (nome === "" || email === "" || mensagem === "") {
 
         status.innerHTML = "Preencha todos os campos.";
         status.style.color = "red";
-
+    //testa se o e-mail é valido
     } else if (!emailValido.test(email)) {
 
         status.innerHTML = "Digite um e-mail válido.";
         status.style.color = "red";
-
+    // Caso todos os requisitos sejam atendidos mostra que enviou na tela e limpa os forms
     } else {
 
         status.innerHTML = "Mensagem enviada com sucesso!";
